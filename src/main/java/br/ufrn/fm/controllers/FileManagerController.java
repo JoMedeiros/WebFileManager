230fd5d125cd2516791a2c6ece1c12054968f883
jomedeiros <josivanmedeiros@lcc.ufrn.br>
2020-11-01 17:36:45 -0300

    esqueleto do método da issue #2

--- src/main/java/br/ufrn/fm/controllers/FileManagerController.java
+++ src/main/java/br/ufrn/fm/controllers/FileManagerController.java
@@ -1,8 +1,11 @@
 package br.ufrn.fm.controllers;
 
+import br.ufrn.fm.models.MoveDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class FileManagerController {
 
+    public static final String root = "storage/";
     /**
      *
      * @param request
@@ -33,6 +37,22 @@ public class FileManagerController {
     @RequestMapping("/root/**/files")
     public String list_from_root(HttpServletRequest request) {
         return "from list_files(), request uri: " + request.getRequestURI();
+    }
+
+    @PutMapping(
+            value = "/api/move",
+            consumes={MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public ResponseEntity<String> move(HttpServletRequest request,
+                                       @RequestBody MoveDetails moveDetails) {
+        String responseTxt = "Moving " + moveDetails.getSourcePath() + " to " + moveDetails.getDestinationPath();
+        // @TODO verificar se os caminhos são válidos
+        boolean validPaths = true;
+        if (validPaths)
+            return ResponseEntity.ok().body(responseTxt);
+        else
+            // @TODO Colocar uma mensagem mais significativa dizendo qual dos caminhos é inválido
+            return ResponseEntity.badRequest().body("Caminho inválido");
     }
 
 }
