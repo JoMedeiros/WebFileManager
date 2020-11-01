401e3cf1309e845d62ec8d631f73ae3def2c90a6
jomedeiros <josivanmedeiros@lcc.ufrn.br>
2020-11-01 18:28:36 -0300

    esqueleto do delete (issue #3)

--- src/main/java/br/ufrn/fm/controllers/FileManagerController.java
+++ src/main/java/br/ufrn/fm/controllers/FileManagerController.java
@@ -1,16 +1,19 @@
 package br.ufrn.fm.controllers;
 
+import br.ufrn.fm.models.DeleteDetails;
 import br.ufrn.fm.models.MoveDetails;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
-@ResponseBody
+@RestController
+@RequestMapping("/api")
+@Api(value = "API REST Para gerenciamento de arquivos remotos.")
 /**
  * Esse link pode ajudar com URI patterns:
  * https://www.logicbig.com/tutorials/spring-framework/spring-web-mvc/uri-pattern.html
@@ -39,8 +42,11 @@ public class FileManagerController {
         return "from list_files(), request uri: " + request.getRequestURI();
     }
 
+    @ApiOperation(value = "Request para mover um arquivo ou diretório",
+            consumes = "Consome um JSON com dois campos: sourcePath e destinationPath. " +
+                    "Move o arquivo do sourcePath para o destinationPath.")
     @PutMapping(
-            value = "/api/move",
+            value = "/move",
             consumes={MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE} )
     public ResponseEntity<String> move(HttpServletRequest request,
@@ -55,4 +61,24 @@ public class FileManagerController {
             return ResponseEntity.badRequest().body("Caminho inválido");
     }
 
+    @ApiOperation(value = "Request para deletar um arquivo ou diretório",
+    consumes = "Consome um JSON com dois campos: path e recusive. O campo path indica o caminho para o arquivo ou " +
+            "diretório a ser deletado e recursive é um booleano que indica se os arquivos serão deletados " +
+            "recursivamente")
+    @DeleteMapping(value = "/delete",
+            consumes={MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public ResponseEntity<String> delete(HttpServletRequest request,
+                                         @RequestBody DeleteDetails deleteDetails) {
+        String responseTxt = "Deleting " + deleteDetails.getPath();
+        if (deleteDetails.isRecursive())
+            responseTxt += " recursively";
+        // @TODO verificar se os caminhos são válidos
+        boolean validPaths = true;
+        if (validPaths)
+            return ResponseEntity.ok().body(responseTxt);
+        else
+            // @TODO Colocar uma mensagem mais significativa dizendo qual dos caminhos é inválido
+            return ResponseEntity.badRequest().body("Caminho inválido");
+    }
 }
